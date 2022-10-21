4ad7f22fe2062804644cf0ab0e5a8a829585def4
Leohearts <leohearts@leohearts.com>
2022-10-21 18:07:44 +0800

    change udev rule to work with 174f:1811 Syntek Integrated Camera

--- system/99-ir-led.rules.cmake
+++ system/99-ir-led.rules.cmake
@@ -1,1 +1,1 @@
-ATTRS{idVendor}=="04f2", ATTRS{idProduct}=="b67c", ACTION=="add", RUN+="@CMAKE_INSTALL_PREFIX@/bin/chicony-ir-toggle on"
+ATTRS{idVendor}=="174f", ATTRS{idProduct}=="1811", ACTION=="add", RUN+="@CMAKE_INSTALL_PREFIX@/bin/chicony-ir-toggle on"
